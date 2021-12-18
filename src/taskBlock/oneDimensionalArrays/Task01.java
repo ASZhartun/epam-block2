2fec62eb13ce12a126aa7fda3859da5ba0ec1765
Anatoliy Zhartun <as.zhartun@yandex.by>
2021-12-18 20:51:50 +0300

    Add description of tasks(block 2). Add empty Task01 at 1d arrays.

---
+++ src/taskBlock/oneDimensionalArrays/Task01.java
@@ -0,0 +1,10 @@
+package taskBlock.oneDimensionalArrays;
+
+/**
+ * 1. В массив A [N] занесены натуральные числа. Найти сумму тех элементов, которые кратны данному К.
+ */
+public class Task01 {
+    public static void main(String[] args) {
+
+    }
+}
